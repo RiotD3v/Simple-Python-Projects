d86e81b7b76d80af476abd58433a4aa39a192b84
Riot <TheRiotDrummer@gmail.com>
2021-01-10 22:24:10 +0000

    default white + erase function

--- paint.py
+++ paint.py
@@ -4,14 +4,19 @@ import pygame
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+black = (0,0,0)
+white = (255,255,255)
 press = False
 
-def draw(screen,x,y):
-	pygame.draw.circle(screen,green,(x,y),10)
+def draw(screen,colour,x,y):
+	pygame.draw.circle(screen,colour,(x,y),10)
 	pygame.display.flip() # update screen
 
 # initialize screen
 screen = pygame.display.set_mode((1000,1000))
+pygame.display.set_caption("Paint")
+
+colour = white
 
 while True:
 	for event in pygame.event.get():
@@ -26,7 +31,16 @@ while True:
 		# draw if mouse down
 		if press == True:
 			(x,y) = pygame.mouse.get_pos()
-			draw(screen,x,y)
+			draw(screen,colour,x,y)
+
+		# key press
+		if event.type == pygame.KEYDOWN:
+
+			if event.key == pygame.K_e:
+				colour = black
+
+			if event.key == pygame.K_d:
+				colour = white
 
 		# exit program
 		if event.type == pygame.QUIT:

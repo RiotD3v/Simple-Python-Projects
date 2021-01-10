0ee06a6220e3f241b502b5069d392ac278b2efd6
Riot <TheRiotDrummer@gmail.com>
2021-01-10 23:45:27 +0000

    add colour settings (unfunctional)

--- paint.py
+++ paint.py
@@ -2,14 +2,25 @@ import pygame
 
 # vars
 red = (255,0,0)
-green = (0,255,0)
+lime = (0,255,0)
 blue = (0,0,255)
+aqua = (0,255,255)
+fuchsia = (255,0,255)
+yellow = (255,255,0)
+navy = (0,0,128)
+teal = (0,128,128)
+green = (0,128,0)
+purple = (128,0,128)
+maroon = (128,0,0)
+olive = (128,128,0)
+silver = (192,192,192)
 black = (0,0,0)
 white = (255,255,255)
 gray = (128,128,128)
 
 press = False
-colour = white
+colour = black
+background = white
 
 def draw(surf,colour,x,y):
 	pygame.draw.circle(surf,colour,(x,y),10)
@@ -22,7 +33,26 @@ pygame.display.set_caption("Paint")
 # surfaces
 drawing = pygame.surface.Surface((700,700))
 settings = pygame.surface.Surface((700,100))
+drawing.fill(background)
 settings.fill(gray)
+
+pygame.draw.rect(settings,red,[25,25,50,50],0) # will display current colour, red as placeholder
+
+pygame.draw.rect(settings,red,[100,20,20,20],0)
+pygame.draw.rect(settings,lime,[100,60,20,20],0)
+pygame.draw.rect(settings,blue,[140,20,20,20],0)
+pygame.draw.rect(settings,aqua,[140,60,20,20],0)
+pygame.draw.rect(settings,fuchsia,[180,20,20,20],0)
+pygame.draw.rect(settings,yellow,[180,60,20,20],0)
+pygame.draw.rect(settings,navy,[220,20,20,20],0)
+pygame.draw.rect(settings,teal,[220,60,20,20],0)
+pygame.draw.rect(settings,green,[260,20,20,20],0)
+pygame.draw.rect(settings,purple,[260,60,20,20],0)
+pygame.draw.rect(settings,maroon,[300,20,20,20],0)
+pygame.draw.rect(settings,olive,[300,60,20,20],0)
+pygame.draw.rect(settings,silver,[340,20,20,20],0)
+pygame.draw.rect(settings,black,[340,60,20,20],0)
+pygame.draw.rect(settings,white,[380,20,20,20],0)
 
 while True:
 	# add surfaces + update
@@ -48,10 +78,10 @@ while True:
 		if event.type == pygame.KEYDOWN:
 
 			if event.key == pygame.K_e:
-				colour = black
+				colour = background
 
 			if event.key == pygame.K_d:
-				colour = white
+				colour = black
 
 		# exit program
 		if event.type == pygame.QUIT:

b65aa688f9ec61d81419285b1d70bdf9edb9cea9
Riot <TheRiotDrummer@gmail.com>
2021-01-10 20:18:31 +0000

    add exit + hold to draw

--- paint.py
+++ paint.py
@@ -1,19 +1,34 @@
 import pygame
 
+# vars
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+press = False
 
 def draw(screen,x,y):
 	pygame.draw.circle(screen,green,(x,y),10)
-	pygame.display.flip()
+	pygame.display.flip() # update screen
 
 # initialize screen
 screen = pygame.display.set_mode((1000,1000))
 
-# draw on mouse click
 while True:
 	for event in pygame.event.get():
+		# mouse down
 		if event.type == pygame.MOUSEBUTTONDOWN:
+			press = True
+
+		# mouse up
+		if event.type == pygame.MOUSEBUTTONUP:
+			press = False
+
+		# draw if mouse down
+		if press == True:
 			(x,y) = pygame.mouse.get_pos()
 			draw(screen,x,y)
+
+		# exit program
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()

ffa33c67f8f8b91b7edca893adbef6ed75fce6d8
Riot <TheRiotDrummer@gmail.com>
2021-01-17 06:27:30 +0000

    add change colour function

--- paint.py
+++ paint.py
@@ -19,12 +19,8 @@ white = (255,255,255)
 gray = (128,128,128)
 
 press = False
-colour = black
+currentcolour = black
 background = white
-
-def draw(surf,colour,x,y):
-	pygame.draw.circle(surf,colour,(x,y),10)
-	pygame.display.flip() # update screen
 
 # initialize screen
 screen = pygame.display.set_mode((700,800))
@@ -36,23 +32,37 @@ settings = pygame.surface.Surface((700,100))
 drawing.fill(background)
 settings.fill(gray)
 
-pygame.draw.rect(settings,red,[25,25,50,50],0) # will display current colour, red as placeholder
+pygame.draw.rect(settings,currentcolour,[25,25,50,50],0) # will display current colour
 
-pygame.draw.rect(settings,red,[100,20,20,20],0)
-pygame.draw.rect(settings,lime,[100,60,20,20],0)
-pygame.draw.rect(settings,blue,[140,20,20,20],0)
-pygame.draw.rect(settings,aqua,[140,60,20,20],0)
-pygame.draw.rect(settings,fuchsia,[180,20,20,20],0)
-pygame.draw.rect(settings,yellow,[180,60,20,20],0)
-pygame.draw.rect(settings,navy,[220,20,20,20],0)
-pygame.draw.rect(settings,teal,[220,60,20,20],0)
-pygame.draw.rect(settings,green,[260,20,20,20],0)
-pygame.draw.rect(settings,purple,[260,60,20,20],0)
-pygame.draw.rect(settings,maroon,[300,20,20,20],0)
-pygame.draw.rect(settings,olive,[300,60,20,20],0)
-pygame.draw.rect(settings,silver,[340,20,20,20],0)
-pygame.draw.rect(settings,black,[340,60,20,20],0)
-pygame.draw.rect(settings,white,[380,20,20,20],0)
+# drawing setting boxes
+redbox = pygame.draw.rect(settings,red,[100,20,20,20],0)
+limebox = pygame.draw.rect(settings,lime,[100,60,20,20],0)
+bluebox = pygame.draw.rect(settings,blue,[140,20,20,20],0)
+aquabox = pygame.draw.rect(settings,aqua,[140,60,20,20],0)
+fuchsiabox = pygame.draw.rect(settings,fuchsia,[180,20,20,20],0)
+yellowbox = pygame.draw.rect(settings,yellow,[180,60,20,20],0)
+navybox = pygame.draw.rect(settings,navy,[220,20,20,20],0)
+tealbox = pygame.draw.rect(settings,teal,[220,60,20,20],0)
+greenbox = pygame.draw.rect(settings,green,[260,20,20,20],0)
+purplebox = pygame.draw.rect(settings,purple,[260,60,20,20],0)
+maroonbox = pygame.draw.rect(settings,maroon,[300,20,20,20],0)
+olivebox = pygame.draw.rect(settings,olive,[300,60,20,20],0)
+silverbox = pygame.draw.rect(settings,silver,[340,20,20,20],0)
+blackbox = pygame.draw.rect(settings,black,[340,60,20,20],0)
+whitebox = pygame.draw.rect(settings,white,[380,20,20,20],0)
+
+boxes = [(redbox,red),(limebox,lime),(bluebox,blue),(aquabox,aqua),(fuchsiabox,fuchsia),
+(yellowbox,yellow),(navybox,navy),(tealbox,teal),(greenbox,green),(purplebox,purple),
+(maroonbox,maroon),(olivebox,olive),(silverbox,silver),(blackbox,black),(whitebox,white)]
+
+def draw(surf,colour,x,y):
+	pygame.draw.circle(surf,colour,(x,y),10)
+	pygame.display.flip() # update screen
+
+def chanegcolour(box,colour,x,y):
+	global currentcolour
+	if box.collidepoint(x,y):
+		currentcolour = colour
 
 while True:
 	# add surfaces + update
@@ -64,6 +74,12 @@ while True:
 		# mouse down
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			press = True
+			x = pygame.mouse.get_pos()[0]
+			y = (pygame.mouse.get_pos()[1])-700
+
+			# check to see if click is to change colour
+			for box in boxes:
+				chanegcolour(box[0],box[1],x,y)
 
 		# mouse up
 		if event.type == pygame.MOUSEBUTTONUP:
@@ -72,7 +88,7 @@ while True:
 		# draw if mouse down
 		if press == True:
 			(x,y) = pygame.mouse.get_pos()
-			draw(drawing,colour,x,y)
+			draw(drawing,currentcolour,x,y)
 
 		# key press
 		if event.type == pygame.KEYDOWN:
